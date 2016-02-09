efd9785ba4ee483bca1c07786260ea8a3735c726
Ragnar Valgeirsson <ragnar.valgeirsson@gmail.com>
2016-02-09 23:34:43 +0000

    Add Google Maps

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,8 @@
 import './modules/router';
+import styles from './data/gmaps-styles';
+
+window.initMap = () => new window.google.maps.Map(document.querySelector('.google-maps'), {
+  center: { lat: 64.1472532, lng: -21.9338615 },
+  zoom: 13,
+  styles,
+});
