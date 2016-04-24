823c79078d95f2d7bab91863885f6689615008b3
Ragnar Thor Valgeirsson <ragnar.valgeirsson@gmail.com>
2016-04-24 23:20:32 +0000

    speakers: added [wip]

---
+++ components/speaker.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Link } from 'react-router'
+import { prefixLink } from 'gatsby-helpers'
+
+export default ({ speaker }) => (
+  <Link
+    className="Speaker"
+    style={{
+      backgroundImage: `url(${require(`!file-loader!../images/speakers/${speaker.slug}.jpg`)})`,
+    }}
+    to={prefixLink(`/speakers/${speaker.slug}`)}
+  >
+    <div className="Speaker-info">
+      <h4 className="Speaker-name">{speaker.name}</h4>
+      <h5 className="Speaker-company">{speaker.company}</h5>
+    </div>
+  </Link>
+)
---
+++ pages/speakers/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import BoxPage from '../../components/box-page'
+import Speaker from '../../components/speaker'
+import speakers from '../../data/speakers'
+
+export default () =>
+  <BoxPage inverse expanded icon="podium" title="Speakers">
+    <div className="SpeakerList">
+      {speakers.map(speaker => <Speaker key={speaker.slug} speaker={speaker} />)}
+    </div>
+  </BoxPage>
