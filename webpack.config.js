031205ef2549aaed6622c600060f7744b9804cf9
Eirikur Nilsson <eirikur@nilsson.is>
2016-04-24 23:01:23 +0000

    Fix travis build

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,7 @@
 // Only used by eslint to find and compile gatsby config.
-/*
- * This file is here until this gets supported:
- */
-try {
-  require('babel-register')({
-    only: /gatsby\/lib/,
-  })
-  const gatsbyConfig = require('./node_modules/gatsby/lib/utils/webpack.config')
+require('babel-register')({
+  only: /gatsby\/lib/,
+})
+const gatsbyConfig = require('./node_modules/gatsby/lib/utils/webpack.config')
 
-  module.exports = gatsbyConfig({}, __dirname, 'develop').resolve()
-} catch (err) {
-  // temporary
-  console.log(err)
-}
+module.exports = gatsbyConfig({}, __dirname, 'develop').resolve()
