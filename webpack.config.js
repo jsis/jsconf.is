1fc074bc9f75a762c812a495fd24ecfc1b3f11e1
Eirikur Nilsson <eirikur@nilsson.is>
2016-04-24 22:56:16 +0000

    Try fix travis npm lint

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,14 @@
 /*
  * This file is here until this gets supported:
  */
-require('babel-register')({
-  only: /gatsby\/lib/,
-})
-const gatsbyConfig = require('./node_modules/gatsby/lib/utils/webpack.config')
+try {
+  require('babel-register')({
+    only: /gatsby\/lib/,
+  })
+  const gatsbyConfig = require('./node_modules/gatsby/lib/utils/webpack.config')
 
-module.exports = gatsbyConfig({}, __dirname, 'develop').resolve()
+  module.exports = gatsbyConfig({}, __dirname, 'develop').resolve()
+} catch (err) {
+  // temporary
+  console.log(err)
+}
