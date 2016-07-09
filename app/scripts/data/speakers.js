14bd93094ad9331ae27a1d3ba974550726ab446f
Aegir Giraldo Thorsteinsson <aegir@thorsteinsson.is>
2016-07-08 22:47:58 -0500

    Change company website for Nick

--- app/scripts/data/speakers.js
+++ app/scripts/data/speakers.js
@@ -221,7 +221,7 @@ const speakers = [{
   twitter: 'mapmeld',
   github: 'mapmeld',
   website: 'http://mapmeld.com',
-  companyWebsite: 'http://asiafoundation.org/',
+  companyWebsite: 'http://www.unicefstories.org/',
   company: 'UNICEF Innovation',
   title: 'If One Laptop per Child were started today, how JavaScript-y would it be?',
   description: `<p>In 2005, it looked like the future of global education might be a custom laptop and OS for $100. Today you could use that money to buy two Android tablets and access eBooks, courses, and programming tutorials on the web. An OLPC hacker reflects on teaching computing in Uganda, Uruguay, and our new JavaScript world.</p>`,
