89bbc0da0a97ab221f22ce6f238e0555b3720dd1
Aegir Giraldo Thorsteinsson <aegir@thorsteinsson.is>
2016-07-13 13:15:45 -0500

    Change company for Amy

--- app/scripts/data/speakers.js
+++ app/scripts/data/speakers.js
@@ -199,8 +199,8 @@ const speakers = [{
   twitter: 'am3thyst',
   github: 'amycheng',
   website: 'http://amycheng.info',
-  companyWebsite: 'https://www.brooklynmuseum.org/',
-  company: 'Brooklyn Museum',
+  companyWebsite: 'http://nymag.com/',
+  company: 'New York Media',
   title: 'Recreate Masterpieces of Modern Art with JavaScript!',
   description: `<p>JavaScript is not just for web apps! This talk looks at JavaScript as an artistic and visual programming language and serves as a primer on generative art. The audience will learn how to recreate one of Damien Hirst’s Spot paintings and one of Piet Mondrian’s Composition paintings in the browser. JavaScript is commonly used to manage interactivity, data, and application states. However, we will use masterpieces of modern art to illustrate JavaScript’s potential to manipulate visual space, color and shape.</p>`,
 }, {
