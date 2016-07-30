0b0addee0853a5a58e835f4dd96f1d5cdb9780a1
Aegir Giraldo Thorsteinsson <aegir@thorsteinsson.is>
2016-07-30 13:40:51 -0700

    Update speakers.js

--- app/scripts/data/speakers.js
+++ app/scripts/data/speakers.js
@@ -335,8 +335,8 @@ const speakers = [{
   twitter: 'sethfsamuel',
   github: 'sethfsamuel',
   website: '',
-  companyWebsite: 'http://www.originate.com',
-  company: 'Originate',
+  companyWebsite: 'https://www.airtime.com/',
+  company: 'Airtime',
   title: 'Arbitrary Computation on the GPU Using WebGL',
   description: `<p>WebGL is already widely in use for 3D graphics and image processing but could be used for much more. We'll learn how to pass arbitrary data to the GPU for parallel processing, how to get that processed data back into Javascript, and all the reasons this doesn't work even when it should.</p>`,
 }, {
