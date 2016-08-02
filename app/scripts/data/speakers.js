e49f4c1824f0cbb8811410010c5ba9e588d48fa6
Eiríkur Heiðar Nilsson <eirikur@nilsson.is>
2016-08-02 15:48:02 -0700

    Myles Borins talk

--- app/scripts/data/speakers.js
+++ app/scripts/data/speakers.js
@@ -29,12 +29,17 @@ const speakers = [{
 }, {
   name: 'Myles Borins',
   slug: 'myles-borins',
-  twitter: '',
-  github: '',
+  twitter: 'TheAlphaNerd',
+  github: 'TheAlphaNerd',
   website: '',
   companyWebsite: '',
   company: '',
-  description: '',
+  title: 'On left-pad and empathy: understanding human connection',
+  description: `
+  <p>Empathy can be a pivotal factor in the success of yourself and your projects. If you don't care about the people using your product, why are you making it? If you don't care about your co-workers who will have to use your code, why do you work with them? If you don't care about yourself, why are you doing this as a career?</p>
+  <p>Some people may ask "why empathy?”. Let’s examine this concept, find ways to be more empathetic in our actions, and discuss the pragmatism of empathy.</p>
+  <p>This talk will explore various ways in which developers can be more empathetic to the community, their co-workers, and themselves. The talk will also explore larger ways we as an industry can work together to improve empathy. We will then use these tools to examine the “left-pad” situation to find empathy for all those involved.</p>
+  `,
 }, {
   name: 'Brian Holt',
   slug: 'brian-holt',
