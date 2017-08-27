eb9ac1fdf4fa83cc1f7cef1d86411b617debdd9f
Ragnar Valgeirsson <ragnar.valgeirsson@gmail.com>
2017-08-27 22:31:14 +0000

    Add basic schedule for next year ❤️

--- data/schedule.js
+++ data/schedule.js
@@ -3,11 +3,148 @@
 
 import speakers from './speakers'
 
-const conferenceDays = []
+const conferenceDays = [
+  {
+    date: 'Thursday March 1, 2018',
+    slots: [
+      {
+        time: '08:00 09:15',
+        unified: {
+          grayed: true,
+          title: 'Breakfast and badge pick-up',
+        },
+      },
+      {
+        time: '09:15 09:45',
+        unified: {
+          grayed: true,
+          title: 'Opening JSConf Iceland 2018',
+        },
+      },
+      {
+        time: '09:45 10:45',
+        unified: {
+          grayed: true,
+          title: 'Talks [TBA]',
+        },
+      },
+      {
+        time: '10:45 11:15',
+        unified: {
+          grayed: true,
+          title: 'Break',
+        },
+      },
+      {
+        time: '11:15 12:30',
+        unified: {
+          grayed: true,
+          title: 'Talks [TBA]',
+        },
+      },
+      {
+        time: '12:30 13:30',
+        unified: {
+          grayed: true,
+          title: 'Lunch',
+        },
+      },
+      {
+        time: '13:30 15:30',
+        unified: {
+          grayed: true,
+          title: 'Talks [TBA]',
+        },
+      },
+      {
+        time: '15:30 16:00',
+        unified: {
+          grayed: true,
+          title: 'Break',
+        },
+      },
+      {
+        time: '16:00 18:00',
+        unified: {
+          grayed: true,
+          title: 'Talks [TBA]',
+        },
+      },
+      {
+        time: '19:00 23:00',
+        unified: {
+          grayed: true,
+          title: 'Party, details TBA',
+        },
+      },
+    ],
+  },
+  {
+    date: 'Friday March 2, 2018',
+    slots: [
+      {
+        time: '08:45 09:45',
+        unified: {
+          grayed: true,
+          title: 'Breakfast',
+        },
+      },
+      {
+        time: '09:45 12:30',
+        unified: {
+          grayed: true,
+          title: 'Talks [TBA]',
+        },
+      },
+      {
+        time: '12:30 13:30',
+        unified: {
+          grayed: true,
+          title: 'Lunch',
+        },
+      },
+      {
+        time: '13:30 15:30',
+        unified: {
+          grayed: true,
+          title: 'Talks [TBA]',
+        },
+      },
+      {
+        time: '15:30 16:00',
+        unified: {
+          grayed: true,
+          title: 'Break',
+        },
+      },
+      {
+        time: '16:00 17:30',
+        unified: {
+          grayed: true,
+          title: 'Talks [TBA]',
+        },
+      },
+      {
+        time: '17:30 18:00',
+        unified: {
+          grayed: true,
+          title: 'Closing remarks',
+        },
+      },
+      {
+        time: '19:00 23:00',
+        unified: {
+          grayed: true,
+          title: 'Party, details TBA',
+        },
+      },
+    ],
+  },
+]
 
 const soDays = []
 
-function trackFor (slug) {
+function trackFor(slug) {
   const speaker = speakers.find(x => x.slug === slug)
   if (!speaker) {
     throw new Error(`Can\'t find speaker ${slug}`)
@@ -20,9 +157,11 @@ function trackFor (slug) {
   }
 }
 
-function speakerToSlot (slot) {
+function speakerToSlot(slot) {
   const timeSlot = slot
-  const keys = Object.keys(slot).filter(x => ['unified', 'Hekla', 'Katla'].indexOf(x) !== -1)
+  const keys = Object.keys(slot).filter(
+    x => ['unified', 'Hekla', 'Katla'].indexOf(x) !== -1
+  )
 
   timeSlot.tracks = keys.map(type => {
     if (typeof slot[type] === 'string') {
@@ -37,7 +176,7 @@ function speakerToSlot (slot) {
   return timeSlot
 }
 
-function mapDays (days) {
+function mapDays(days) {
   return days.map(day_ => {
     const day = day_
     day.slots = day.slots.map(speakerToSlot)
