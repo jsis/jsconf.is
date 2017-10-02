e99db420e240a2b1ee4a87981daa50f49e18548c
Ragnar Valgeirsson <ragnar.valgeirsson@gmail.com>
2017-10-02 22:04:54 +0000

    I miss prettier

--- pages/about/contact.jsx
+++ pages/about/contact.jsx
@@ -4,5 +4,6 @@ import icon from '!svg-inline!../../images/logo-clipped.svg'
 
 export default () =>
   <BoxPage icon={icon} title="Contact us">
-    The JSConf team can be reached through our <a href="https://twitter.com/jsconfis">Twitter</a> account, through <a href="mailto:team@jsconf.is">email</a> or at our meeting place at  <address>Aranja's offices in Nóatún 17, 105 Reykjavík, Iceland.</address>
+    The JSConf team can be reached through our <a href="https://twitter.com/jsconfis">Twitter</a> account, through <a href="mailto:team@jsconf.is">email</a> or at our meeting place at
+    <address>Aranja's offices in Nóatún 17, 105 Reykjavík, Iceland.</address>
   </BoxPage>
