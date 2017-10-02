5893f5ad814c03c0a302b2f92fa34b243ddbd2c6
Ragnar Valgeirsson <ragnar.valgeirsson@gmail.com>
2017-10-02 21:55:44 +0000

    Add contact page
    /about/contact/

---
+++ pages/about/contact.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import BoxPage from '../../components/box-page'
+import icon from '!svg-inline!../../images/logo-clipped.svg'
+
+export default () =>
+  <BoxPage icon={icon} title="Contact us">
+    The JSConf team can be reached through our <a href="https://twitter.com/jsconfis">Twitter</a> account, through <a href="mailto:team@jsconf.is">email</a> or at our meeting place at  <address>Aranja's offices in Nóatún 17, 105 Reykjavík, Iceland.</address>
+  </BoxPage>
