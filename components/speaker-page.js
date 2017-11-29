48d85f9ea7658d55b9b9f9abd8a8309f310c8dd8
Ragnar Valgeirsson <ragnar.valgeirsson@gmail.com>
2017-11-29 15:50:24 +0000

    Add bio and fix speaker page styles

--- components/speaker-page.js
+++ components/speaker-page.js
@@ -1,10 +1,9 @@
 import React from 'react'
 import Helmet from 'react-helmet'
-import BoxPage from './box-page'
-import Profile from './profile'
+import './speaker-page.scss'
 
 const SpeakerPage = ({
-  title,
+  title: name,
   company,
   companyWebsite,
   slug,
@@ -18,22 +17,15 @@ const SpeakerPage = ({
 }) => {
   // eslint-disable-next-line
   const profilePic = require(`../images/speakers/${slug}.jpg`)
-  const boxPageProps = {
-    title,
-  }
-
-  if (company) {
-    boxPageProps.subtitle = companyWebsite ? (
-      <a href={companyWebsite}>{company}</a>
-    ) : (
-      company
-    )
-  }
 
   return (
-    <BoxPage {...boxPageProps}>
+    <section className="Page is-open SpeakerPage">
       <Helmet
         meta={[
+          {
+            property: 'og:title',
+            content: `${name} - JSConf Iceland 2018`,
+          },
           { property: 'og:url', content: `https://2018.jsconf.is${path}/` },
           { property: 'og:description', content: bio },
           {
@@ -51,6 +43,10 @@ const SpeakerPage = ({
             content: '630',
           },
           {
+            name: 'twitter:title',
+            content: `${name} - JSConf Iceland 2018`,
+          },
+          {
             name: 'twitter:image',
             content: `https://2018.jsconf.is/assets/social/speakers/${
               slug
@@ -59,16 +55,74 @@ const SpeakerPage = ({
           { name: 'twitter:description', content: bio },
         ]}
       />
-      <Profile
-        image={profilePic}
-        name={title}
-        twitter={twitter}
-        website={website}
-        github={github}
-      />
-      <h2>{talk}</h2>
-      <div dangerouslySetInnerHTML={{ __html: body }} />
-    </BoxPage>
+      <header className="SpeakerPage-header">
+        <div className="SpeakerPage-info">
+          <h1 className="SpeakerPage-name">{name}</h1>
+          {company &&
+            (companyWebsite ? (
+              <a href={companyWebsite} className="SpeakerPage-company">
+                {company}
+              </a>
+            ) : (
+              <span className="SpeakerPage-company">{company}</span>
+            ))}
+        </div>
+        <div className="SpeakerPage-imageWrap">
+          <img className="SpeakerPage-image" src={profilePic} alt={name} />
+        </div>
+      </header>
+
+      <div className="SpeakerPage-talk">
+        <div className="row align-center">
+          <div className="column xxlarge-8 xlarge-9 large-10 med-11 small-12">
+            <h2 className="SpeakerPage-talkTitle">{talk}</h2>
+            <div
+              className="SpeakerPage-abstract"
+              dangerouslySetInnerHTML={{ __html: body }}
+            />
+          </div>
+        </div>
+      </div>
+
+      <aside>
+        <div className="row align-center">
+          <div className="column xxlarge-8 xlarge-9 large-10 med-11 small-12">
+            <h3>About {name}</h3>
+            {bio && <p>{bio}</p>}
+            {(website || twitter || github) && (
+              <div>
+                <h4 className="SpeakerPage-socialTitle">
+                  Find {name.split(' ')[0]} online at:
+                </h4>
+                <ul className="SpeakerPage-links">
+                  {website && (
+                    <li className="SpeakerPage-link">
+                      <a href={website}>
+                        {website.replace(/^(https?:)?\/\//, '')}
+                      </a>
+                    </li>
+                  )}
+                  {twitter && (
+                    <li className="SpeakerPage-link">
+                      <a href={`https://twitter.com/${twitter}`}>
+                        twitter.com/{twitter}
+                      </a>
+                    </li>
+                  )}
+                  {github && (
+                    <li className="SpeakerPage-link">
+                      <a href={`https://github.com/${github}`}>
+                        github.com/{github}
+                      </a>
+                    </li>
+                  )}
+                </ul>
+              </div>
+            )}
+          </div>
+        </div>
+      </aside>
+    </section>
   )
 }
 
