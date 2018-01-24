f3e393d0d775790bec4897cd8d56ef95b722c3c9
Ragnar Valgeirsson <ragnar.valgeirsson@gmail.com>
2018-01-24 22:27:34 +0000

    Add slots

--- data/schedule.js
+++ data/schedule.js
@@ -2,10 +2,23 @@
 'use strict'
 
 const description =
-  '<p>We are inviting the JavaScript community to submit talks for JSConf Iceland 2018.</p><p>For more information <a target="_blank" href="http://cfp.jsconf.is/events/jsconf-iceland-2018">click' +
+  '<p>Check out the speaker lineup <a href="/speakers/">' +
   ' here</a>.</p>'
 
 const conferenceDays = [
+  {
+    date: 'Wednesday February 28, 2018',
+    slots: [
+      {
+        time: '20:00 23:00',
+        unified: {
+          grayed: true,
+          title: 'Welcome party',
+          description: '',
+        },
+      },
+    ],
+  },
   {
     date: 'Thursday March 1, 2018',
     slots: [
@@ -144,11 +157,35 @@ const conferenceDays = [
   },
   {
     date: 'Saturday March 3, 2018',
-    slots: [],
+    slots: [
+      {
+        time: '10:00 18:00',
+        unified: {
+          title: 'South Shore Adventure',
+          description:
+          '<p><img src="/assets/south-coast.jpg" alt="The south shore adventure"/></p>'+
+          '<p>This tour is ideal for nature lovers of all kinds. We take you along the south coast of Iceland, one of the country’s most scenic regions, as far as Vík, a charming village surrounded by high beautiful bird cliffs.</p>' +
+          '<p>South Iceland is special for that it is one of the flattest regions on the island. Because of that, most of all grain farming in Iceland is done there. The high mountains to the north, shield the area from the harshest winds from the north.</p>' +
+          '<p>Among the stops on this tour are at the gorgeous Seljalandsfoss and Skógafoss waterfalls. Seljalandsfoss is part of the Seljandsá river, which has its origins in Eyjafjallajökull glacier. What makes Seljalandsfoss so famous and interesting is that you can walk behind it. We recommend you bring either a raincoat or a waterproof coat along if you want to walk behind it. Skógafoss, another famous Icelandic waterfall, is the starting point of the Fimmvörðuháls walk over to Þórsmörk. The waterfall was also used in the films <em>Thor: The Dark World</em> and <em>The Secret Life of Walter Mitty</em>.</p>' +
+          '<p>We will also stop at Reynisfjara black sand beach, with the Reynisdrangar rock formations and columnar basalt. It is one of the most spectacular beaches in Iceland but can be dangerous, and we kindly ask our guests not to go too close to the ocean and go by guidelines.</p>' +
+          '<p>Tour duration: 10:00 – 18:00</p>' +
+          '<p>On this tour you will see:<br>• Sólheimajökull<br>• Vík<br>• Reynisfjara<br>• Reynisdrangar<br>• Skógafoss<br>• Seljalandsfoss</p>'
+        },
+      },
+    ],
   },
   {
     date: 'Sunday March 4, 2018',
-    slots: [],
+    slots: [
+      {
+        time: '09:00 14:00',
+        unified: {
+          grayed: true,
+          title: 'Blue Lagoon',
+          description: '',
+        }
+      }
+    ],
   },
 ]
 
