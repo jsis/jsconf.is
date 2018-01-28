8e52b8ffe8019f790004e40970bfa9b84684fe67
Ragnar Þór Valgeirsson <ragnar.valgeirsson@gmail.com>
2018-01-28 23:20:58 +0000

    Add all days to SO tracks

--- data/schedule.js
+++ data/schedule.js
@@ -5,6 +5,33 @@ const description =
   '<p>Check out the speaker lineup <a href="/speakers/">' +
   ' here</a>.</p>'
 
+const southShore = {
+  time: '10:00 19:00',
+  unified: {
+    title: 'South Shore Adventure',
+    description:
+    '<p><img src="/assets/south-coast.jpg" alt="The south shore adventure"/></p>'+
+    '<p>This tour is ideal for nature lovers of all kinds. We take you along the south coast of Iceland, one of the country’s most scenic regions, as far as Vík, a charming village surrounded by high beautiful bird cliffs.</p>' +
+    '<p>South Iceland is special for that it is one of the flattest regions on the island. Because of that, most of all grain farming in Iceland is done there. The high mountains to the north, shield the area from the harshest winds from the north.</p>' +
+    '<p>Among the stops on this tour are at the gorgeous Seljalandsfoss and Skógafoss waterfalls. Seljalandsfoss is part of the Seljandsá river, which has its origins in Eyjafjallajökull glacier. What makes Seljalandsfoss so famous and interesting is that you can walk behind it. We recommend you bring either a raincoat or a waterproof coat along if you want to walk behind it. Skógafoss, another famous Icelandic waterfall, is the starting point of the Fimmvörðuháls walk over to Þórsmörk. The waterfall was also used in the films <em>Thor: The Dark World</em> and <em>The Secret Life of Walter Mitty</em>.</p>' +
+    '<p>We will also stop at Reynisfjara black sand beach, with the Reynisdrangar rock formations and columnar basalt. It is one of the most spectacular beaches in Iceland but can be dangerous, and we kindly ask our guests not to go too close to the ocean and go by guidelines.</p>' +
+    '<p>Tour duration: 10:00 – 19:00</p>' +
+    '<p>On this tour you will see:<br>• Sólheimajökull<br>• Vík<br>• Reynisfjara<br>• Reynisdrangar<br>• Skógafoss<br>• Seljalandsfoss</p>' +
+    '<p>Tickets are sold separately for both attendees and SOs. Attendees will receive a link via email to buy tickets.</p>',
+  },
+}
+
+const blueLagoon = {
+  time: '09:00 14:00',
+  unified: {
+    title: 'Brunch at the Blue Lagoon',
+    description:
+    '<p>Enjoy brunch at the Blue Lagoon as a final gathering after the conference. Then relax in the lagoon as long as you like. Includes transportation from city center, brunch buffet, and access to the lagoon.</p>' +
+    '<p>The buses leave at 9am, the brunch starts at 10am and access to the lagoon starts at 11am.</p>' +
+    '<p>Tickets are sold separately for both attendees and SOs. Attendees will receive a link via email to buy tickets.</p>',
+  },
+}
+
 const conferenceDays = [
   {
     date: new Date('Wednesday February 28, 2018'),
@@ -158,42 +185,34 @@ const conferenceDays = [
   {
     date: new Date('Saturday March 3, 2018'),
     slots: [
-      {
-        time: '10:00 19:00',
-        unified: {
-          title: 'South Shore Adventure',
-          description:
-          '<p><img src="/assets/south-coast.jpg" alt="The south shore adventure"/></p>'+
-          '<p>This tour is ideal for nature lovers of all kinds. We take you along the south coast of Iceland, one of the country’s most scenic regions, as far as Vík, a charming village surrounded by high beautiful bird cliffs.</p>' +
-          '<p>South Iceland is special for that it is one of the flattest regions on the island. Because of that, most of all grain farming in Iceland is done there. The high mountains to the north, shield the area from the harshest winds from the north.</p>' +
-          '<p>Among the stops on this tour are at the gorgeous Seljalandsfoss and Skógafoss waterfalls. Seljalandsfoss is part of the Seljandsá river, which has its origins in Eyjafjallajökull glacier. What makes Seljalandsfoss so famous and interesting is that you can walk behind it. We recommend you bring either a raincoat or a waterproof coat along if you want to walk behind it. Skógafoss, another famous Icelandic waterfall, is the starting point of the Fimmvörðuháls walk over to Þórsmörk. The waterfall was also used in the films <em>Thor: The Dark World</em> and <em>The Secret Life of Walter Mitty</em>.</p>' +
-          '<p>We will also stop at Reynisfjara black sand beach, with the Reynisdrangar rock formations and columnar basalt. It is one of the most spectacular beaches in Iceland but can be dangerous, and we kindly ask our guests not to go too close to the ocean and go by guidelines.</p>' +
-          '<p>Tour duration: 10:00 – 19:00</p>' +
-          '<p>On this tour you will see:<br>• Sólheimajökull<br>• Vík<br>• Reynisfjara<br>• Reynisdrangar<br>• Skógafoss<br>• Seljalandsfoss</p>' +
-          '<p>Tickets are sold separately for both attendees and SOs. Attendees will receive a link via email to buy tickets.</p>',
-        },
-      },
+      southShore,
     ],
   },
   {
     date: new Date('Sunday March 4, 2018'),
     slots: [
-      {
-        time: '09:00 14:00',
-        unified: {
-          title: 'Brunch at the Blue Lagoon',
-          description:
-          '<p>Enjoy brunch at the Blue Lagoon as a final gathering after the conference. Then relax in the lagoon as long as you like. Includes transportation from city center, brunch buffet, and access to the lagoon.</p>' +
-          '<p>The buses leave at 9am, the brunch starts at 10am and access to the lagoon starts at 11am.</p>' +
-          '<p>Tickets are sold separately for both attendees and SOs. Attendees will receive a link via email to buy tickets.</p>',
-        },
-      },
+      blueLagoon,
     ],
   },
 ]
 
 
 const soDays = [
+  {
+    date: new Date('Wednesday February 28, 2018'),
+    slots: [
+      {
+        time: '20:00 23:59',
+        unified: {
+          title: 'Welcome Reception at Hard Rock',
+          description: '<p>Come to the welcome reception, meet conference attendees and other' +
+          ' SOs at Hard Rock. The reception is situated on the famous Lækjargata, in the heart' +
+          ' of Reykjavik City. Hard Rock Cafe Reykjavik contains three floors, each with its own distinctive vibe.</p>' +
+          '<p>We look forward to seeing you!</p>',
+        },
+      },
+    ],
+  },
   {
     date: new Date('Thursday March 1, 2018'),
     slots: [
@@ -266,6 +285,18 @@ const soDays = [
           title: 'Arrive back to Harpa.',
         },
       },
+    ],
+  },
+  {
+    date: new Date('Saturday March 3, 2018'),
+    slots: [
+      southShore,
+    ],
+  },
+  {
+    date: new Date('Sunday March 4, 2018'),
+    slots: [
+      blueLagoon,
     ],
   },
 ]
