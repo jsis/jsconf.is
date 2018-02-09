c8e5e2851386a9c61fff5d19d842e1b1be65c22c
Ragnar Valgeirsson <ragnar.valgeirsson@gmail.com>
2018-02-09 17:01:43 +0000

    Lint

--- data/speakers.js
+++ data/speakers.js
@@ -34,7 +34,7 @@ const speakers = [
     company: '',
     title: 'Node.js: Where are we now, where are we going?',
     description: `<p>Node.js has been around for 9 years now, so you might think we’re getting closer to holding a finished piece of software in our hands. Yet somehow, despite placing a high value on stability, it is seeing a record number of commits and new contributors.</p><p>With the Node 10.0.0 release just around the corner, it’s worth taking a closer look at what has been changing and where Node.js is headed: HTTP/2 support, yS Modules, N-API, async_hooks and a Promise-based standard library are just some of the upcoming gems that this talk will put in the spotlight.</p>`,
-    bio: 'Over the last 2 years, Anna has been one of the most active contributors to Node.js core. Currently pursuing a Master’s degree in mathematics in Germany, she is fueled by a passion for Node and its community.'
+    bio: 'Over the last 2 years, Anna has been one of the most active contributors to Node.js core. Currently pursuing a Master’s degree in mathematics in Germany, she is fueled by a passion for Node and its community.',
   },
   {
     name: 'David Khourshid',
