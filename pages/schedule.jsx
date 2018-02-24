3bc9e4029aa59a7a425a6aa4a620aedd1e25b14f
Ragnar Valgeirsson <ragnar.valgeirsson@gmail.com>
2018-02-24 18:35:18 +0000

    Set correct meta titles for Schedule

--- pages/schedule.jsx
+++ pages/schedule.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Helmet from 'react-helmet'
 import BoxPage from '../components/box-page'
 import * as schedule from '../data/schedule'
 import Waypoint from 'react-waypoint'
@@ -17,6 +18,19 @@ class Schedule extends React.Component {
     const { footerPosition } = this.state
     return (
       <BoxPage expanded inverse>
+        <Helmet
+          title="Schedule"
+          meta={[
+            {
+              property: 'og:title',
+              content: 'Schedule - JSConf Iceland 2018',
+            },
+            {
+              name: 'twitter:title',
+              content: 'Schedule - JSConf Iceland 2018',
+            },
+          ]}
+        />
         <Events
           conference={schedule.conference}
           so={schedule.so}
