2ac3350302947c3672913eac363c3552609e2877
Ragnar Valgeirsson <ragnar.valgeirsson@gmail.com>
2018-02-28 15:18:18 +0000

    Add Reporting section to the code of conduct page

--- pages/code-of-conduct.jsx
+++ pages/code-of-conduct.jsx
@@ -21,5 +21,22 @@ export default () => (
     </dl>
 
     <p>In case of any doubt, please ask before taking photographs of attendees, speakers or staff.</p>
+
+    <h2>Reporting</h2>
+    <p>If someone makes you or anyone else feel unsafe or unwelcome, please report it as soon as possible. Conference staff can be identified by t-shirts. Harassment and other Code of Conduct violations reduce the value of our event for everyone. We want you to be happy at our event. People like you make our event a better place. You can make a report either personally or anonymously.</p>
+
+    <p>You can make both named and anonymous <a href="https://jsconfis.typeform.com/to/Wf7D2N" target="_blank">report here</a>. We can’t follow up an anonymous report with you directly, but we will fully investigate it and take whatever action is necessary to prevent a recurrence.</p>
+
+    <p>If you need help or assistance during the conference, you can make a personal report by:</p>
+    <ul>
+      <li>Contacting a staff member, identified by organizer's t-shirts.</li>
+      <li>Emailing us: <a href="mailto:team@jsconf.is">team@jsconf.is</a>.</li>
+      <li>Calling us: <a href="tel:+3547751787">(+354) 775-1787</a></li>
+    </ul>
+
+    <p>Our team will be happy to help you contact hotel/venue security, local law enforcement, local support services, provide escorts, or otherwise assist you to feel safe for the duration of the event. We value your attendance.</p>
+
+    <p><strong>Important contact numbers:</strong></p>
+    <p>For police and medical emergencies, call <a href="tel:112">112</a>.</p>
   </BoxPage>
 )
